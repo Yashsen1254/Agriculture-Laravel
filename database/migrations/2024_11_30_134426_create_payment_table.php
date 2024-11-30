9a4a86c120b70ee1b604715b51137691f9102011
Yash Sen <yashsen0402@gmail.com>
2024-11-30 19:42:55 +0530

    DB Updated

--- database/migrations/2024_11_30_134426_create_payment_table.php
+++ database/migrations/2024_11_30_134426_create_payment_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('payment', function (Blueprint $table) {
             $table->id('Id');
-            $table->string('Received');
             $table->unsignedBigInteger('Orderid');
             $table->foreign('Orderid')->references('Id')->on('order');
             $table->timestamps();
